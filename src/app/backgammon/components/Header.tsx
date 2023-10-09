561a9de8725b9dbbcef622440962d969011462ae
anton <antonjollant@aol.com>
2023-10-09 15:08:17 +0200

    removed header in backgammon

--- src/app/backgammon/components/Header.tsx
+++ src/app/backgammon/components/Header.tsx
@@ -10,36 +10,12 @@ export function Header() {
     return (
         <div className="header">
             <h3>{gameState.errorMessage}</h3>
+            <div className="site-info-header">
+                <h1>React Backgammon</h1>
+            </div>
             <div className="game-info-header">
                 <h3 id="turn-indicator">{gameState.turn}&apos; turn, </h3>
                 <Dice />
-            </div>
-            <div className="site-info-header">
-                <h1>React Backgammon</h1>
-                <div>
-                    <a
-                        href="https://github.com/JennaHuff/DragAndDrop-Test"
-                        target="_blank"
-                    >
-                        <button className="header-button">
-                            <Image
-                                src={githubLogo}
-                                alt="github repository link"
-                            />
-                        </button>
-                    </a>
-                    <a
-                        href="https://github.com/JennaHuff/DragAndDrop-Test/blob/main/README.md"
-                        target="_blank"
-                    >
-                        <button className="header-button">
-                            <Image
-                                src={infoIcon}
-                                alt="more information about this site"
-                            />
-                        </button>
-                    </a>
-                </div>
             </div>
         </div>
     );
