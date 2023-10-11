c541464186a7ab4b362f6e0394e600f8fba9a837
anton <antonjollant@aol.com>
2023-10-11 15:12:36 +0200

    changed backgammon pawns and favicon

--- src/app/backgammon/components/Pawn.tsx
+++ src/app/backgammon/components/Pawn.tsx
@@ -1,40 +1,27 @@
-import { DragPreviewImage, useDrag } from "react-dnd";
+import { useDrag } from "react-dnd";
 import { ISquare } from "../utils/types";
-
-function SvgPiece({ color }: { color: ISquare["color"] }) {
-    return (
-        <svg
-            height="40px"
-            viewBox="-10 -10 120 120"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <circle
-                cy="40"
-                cx="40"
-                r="40"
-                fill={color}
-                stroke={color === "white" ? "black" : "white"}
-                strokeWidth="10"
-            />
-        </svg>
-    );
-}
+import BlackPawn from "/public/black-pawn.svg";
+import WhitePawn from "/public/white-pawn.svg";
+import Image from "next/image";
 
 export function Pawn({ square }: { square: ISquare }) {
-    const [, drag, preview] = useDrag(() => ({
+    const [, drag] = useDrag(() => ({
         type: "pawn",
         item: square,
     }));
 
     return (
         <>
-            <DragPreviewImage connect={preview} src={SvgPiece.toString()} />
+            {/* <DragPreviewImage connect={preview} src={svgString} /> */}
             <div
                 ref={drag}
                 className="clickable"
                 style={{ transform: "translate(0, 0)" }}
             >
-                <SvgPiece color={square.color} />
+                <Image
+                    alt={`${square.color === "white" ? "white" : "black"} pawn`}
+                    src={square.color === "white" ? WhitePawn : BlackPawn}
+                />
             </div>
         </>
     );
